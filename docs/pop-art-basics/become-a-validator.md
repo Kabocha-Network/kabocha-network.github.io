e1d74413300d93d89e7e98ad3416d4537d7ee204
decentration <ramseyajram@me.com>
2022-04-21 15:00:56 +0100

    navbar links

--- docs/pop-art-basics/become-a-validator.md
+++ docs/pop-art-basics/become-a-validator.md
@@ -6,9 +6,9 @@ sidebar_position: 2
 
 ![alt text](https://github.com/Kabocha-Network/polkadot/blob/master/images/pop-art.jpg?raw=true?raw=true)
 
-**Repo**: https://github.com/kabocha-network/relay-chain/edit/v0.9.16-n1/  
+**Repo**: https://github.com/kabocha-network/relay-chain/tree/v0.9.18#pop-art-relay-chain   
 
-**Branch**: v0.9.16-n1
+**Branch**: v0.9.18
 
 This branch contains Pop-Art, a custom Rococo relay staging network. It is intended for projects in the Substrate ecosytem (and Edgeware/Kabocha community), so that people can test their parachain integrations, and get experiecne as a validator in a shared network. 
 
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
 
 
 const FeatureList = [
   {
     title: 'Decentralised',
-    Svg: require('@site/static/img/pumpkin-orange.svg').default,
+    // Svg: require('@site/static/img/pumpkin-orange.svg').default,
+    imageUrl: 'img/seedOne.jpg',
     description: (
       <>
         Decentration builds decentralised open-source technologies and blockchain networks. 
@@ -15,16 +18,20 @@ const FeatureList = [
   },
   {
     title: 'Holistic ',
-    Svg: require('@site/static/img/pumpkin-green.svg').default,
+    // Svg: require('@site/static/img/pumpkin-green.svg').default,
+    imageUrl: 'img/seed1.jpg',
     description: (
       <>
         Our approach to building is holistic, humanistic, focussing on values. 
       </>
     ),
+    pageUrl: "https://wiki.kabocha.network",
+    linkName: "Learn More",
   },
   {
     title: 'Sovereign',
-    Svg: require('@site/static/img/pumpkin-yellow.svg').default,
+    // Svg: require('@site/static/img/pumpkin-yellow.svg').default,
+    imageUrl: 'img/seed2.jpg',
     description: (
       <>
         Sovereign, self-evolving, self-governing, self-determining. 
@@ -33,15 +40,21 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({imageUrl, title, description, pageUrl, linkName}) {
+  const imgUrl = useBaseUrl(imageUrl);
   return (
+
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+        {/* <img className={styles.featureSvg} src={SeedOne} role="img" /> */}
+        <img className={styles.featureSvg} src={imgUrl} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h3 className={styles.descriptStyle}>{title}</h3>
         <p>{description}</p>
+        <p className={styles.learnMore}><a href={pageUrl}>{linkName}</a></p>
+        
       </div>
     </div>
   );
